451d7411b3646351ccc2ae9e5a11e6e4c1796f1f
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-08-14 21:39:47 +0300

    exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props);
   return (
     <div>
       <h1>{props.course}</h1>
@@ -7,6 +8,8 @@ const Header = (props) => {
 };
 
 const Part = (props) => {
+  console.log(props);
+
   return (
     <div>
       <p>
@@ -17,6 +20,8 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
+  console.log(props);
+
   return (
     <div>
       <Part part={props.part} exercises={props.exercises} />
@@ -27,6 +32,8 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
+  console.log(props);
+
   return (
     <div>
       <p>
@@ -39,28 +46,34 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part={part1}
-        exercises={exercises}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
+        part={part1.name}
+        exercises={part1.exercises}
+        part2={part2.name}
+        exercises2={part2.exercises}
+        part3={part3.name}
+        exercises3={part3.exercises}
       />
       <Total
-        exercises={exercises}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
       />
     </div>
   );
