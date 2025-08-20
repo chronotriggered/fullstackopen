bf4bc95ab427b33e0a46bf4a26a9abb9c2cc1232
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-08-20 17:05:49 +0300

    whole part1 updated

--- part1/p1_d/p1_d_examples/src/App.jsx
+++ part1/p1_d/p1_d_examples/src/App.jsx
@@ -1,26 +1,23 @@
-import { useState } from "react"; // Import useState hook from React to manage state
+import { useState } from "react";
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
+
+const Display = (props) => <div>{props.value}</div>;
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(" ")}</p>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
