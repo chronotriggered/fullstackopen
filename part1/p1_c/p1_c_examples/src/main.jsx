d7b89a7483c8956c8dcd669cb1b1a1b0831a7630
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-08-20 17:07:18 +0300

    removed extra stuff

--- part1/p1_c/p1_c_examples/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client"; // Import ReactDOM for rendering React components
-
-import App from "./App"; // Import the App component from App.jsx
-let counter = 1;
-
-// render the App component to the root element that has the id "root"
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- part1/p1_d/p1_d_examples/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom/client"; // Import ReactDOM for rendering React components
-
-import App from "./App"; // Import the App component from App.jsx
-
-// render the App component to the root element that has the id "root"
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
