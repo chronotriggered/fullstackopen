beb32497c57d38248c09e5672a41e5593ded3b34
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-08-28 10:22:43 +0300

    phonebook step 7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import newService from "./services/persons";
 
 const Filter = ({ value, onChange }) => {
   return (
@@ -41,10 +41,12 @@ const App = () => {
   const [newFilter, setNewFilter] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    newService.getAll().then((response) => {
+      setPersons(response);
     });
   }, []);
+
+  console.log(persons);
 
   const filterResults = persons.filter((person) =>
     person.name.toLowerCase().includes(newFilter)
