9c7df92c9f4418d6dee2d324885bec68783fc926
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-10-29 21:40:25 +0200

    part3 update

--- part3/exercises3/index.js
+++ part3/exercises3/index.js
@@ -1,27 +1,43 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 let notes = [
+  { id: 1, content: "HTML is easy", important: true },
+  { id: 2, content: "Browser can execute only JavaScript", important: false },
   {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
+    id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true,
   },
+  {
+    id: 4,
+    content: "Express is a framework for building web applications in Node.js",
+    important: true,
+  },
+  {
+    id: 5,
+    content:
+      "Middleware functions are functions that have access to the request object",
+    important: false,
+  },
+  {
+    id: 6,
+    content: "The app.listen function starts a server",
+    important: true,
+  },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+
+app.get("/", (request, response) => {
+  // '/' in this context means the root route, i.e., http://localhost:3001/
+  response.send("<h1>Hello World!</h1>"); // send() method sends the HTTP response back to the client which is making the request
+});
+
+app.get("/api/notes", (request, response) => {
+  // '/api/notes' is the route for accessing notes (http://localhost:3001/api/notes)
+  response.json(notes); // json method sends a JSON response to the client (same as res.send() but also sets the Content-Type header to application/json
 });
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
