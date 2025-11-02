ac4c44e0da1dfbc98a8cd3887a688025635d71fd
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-11-02 22:02:15 +0200

    render build

--- part3/pb-backend/index.js
+++ part3/pb-backend/index.js
@@ -91,6 +91,10 @@ app.post(
   }
 );
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
