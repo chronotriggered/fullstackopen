c6da36b8fe5232493b9411bb1416c171724ef8ee
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-11-02 21:56:45 +0200

    render build

--- part3/pb-backend/index.js
+++ part3/pb-backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
+app.use(express.static("build"));
 
 morgan.token("type", (req, res) => JSON.stringify(req.body));
 
