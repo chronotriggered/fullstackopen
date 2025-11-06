c432736887a1169836a8c5d7d1c483ce3a224d34
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-11-06 21:38:42 +0200

    uibuild

--- part3/pb-backend/index.js
+++ part3/pb-backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const Entry = require("./models/person");
 const path = require("path");
+const e = require("express");
 const app = express();
 
 app.use(express.json());
@@ -14,29 +15,6 @@ morgan.token("type", (req, res) => JSON.stringify(req.body));
 
 app.use(morgan("tiny", { skip: (req, res) => req.method === "POST" }));
 
-let pb_entries = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons/", (request, response) => {
   Entry.find({}).then((entry) => {
     response.json(entry);
@@ -44,55 +22,67 @@ app.get("/api/persons/", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  const entries_amount = pb_entries.length;
-  const request_date = new Date();
-  response.send(
-    `<p>Phonebook has info for ${entries_amount} people</p><p>${request_date}</p>`
-  );
+  Entry.countDocuments({}).then((entries_amount) => {
+    const request_date = new Date();
+    response.send(
+      `<p>Phonebook has info for ${entries_amount} people</p><p>${request_date}</p>`
+    );
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const entry = pb_entries.find((e) => e.id === id);
-  if (entry) {
-    response.json(entry);
-  } else {
-    response.status(204).end();
-  }
+  Entry.findById(id).then((entry) => {
+    if (entry) {
+      response.json(entry);
+    } else {
+      response.status(204).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  pb_entries = pb_entries.filter((e) => e.id !== id);
-  response.status(204).end();
+  Entry.findByIdAndRemove(id).then(() => {
+    response.status(204).end();
+  });
 });
 
 app.post(
   "/api/persons/",
   morgan(":method :url :status :res[content-length] - :response-time ms :type"),
   (request, response) => {
+    // request body comes from express.json() middleware
+    // request body contains the data sent by the client
+    // in this case, the data is in json format and contains name and number
     const body = request.body;
-    const entry = {
-      id: Math.floor(Math.random() * 100000).toString(),
-      name: body.name,
-      number: body.number,
-    };
-
-    const name_in_use = pb_entries.find((e) => e.name === body.name);
-    const number_in_use = pb_entries.find((e) => e.number === body.number);
 
     if (!body.name) {
       return response.status(400).json({ error: "name is missing" });
     } else if (!body.number) {
       return response.status(400).json({ error: "number is missing" });
-    } else if (name_in_use) {
-      return response.status(400).json({ error: "name must be unique" });
-    } else if (number_in_use) {
-      return response.status(400).json({ error: "number must be unique" });
-    } else {
-      pb_entries = pb_entries.concat(entry);
-      response.status(201).json(entry);
     }
+
+    // entry with small e refers to the mongoose model
+    // Entry with big E refers to the model imported at the top
+    // model in mongoose is like a table in sql
+    // each instance of a model is like a row in sql
+    // sql is relational, mongoose is non-relational
+    const entry = new Entry({
+      name: body.name,
+      number: body.number,
+    });
+
+    // sending back the saved entry as json to the client
+    // client could be postman, browser, react app, etc.
+    // axios automatically converts json to js object for the react app
+    // callback function without arrow is made like this:
+    // function(savedEntry) { response.json(savedEntry); }
+    // function name in this case is null because it's an anonymous function
+    // saevedEntry is the entry after being saved to the database
+    entry.save().then((savedEntry) => {
+      response.json(savedEntry);
+    });
   }
 );
 
